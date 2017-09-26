827e22373699047936f675ee93990efe336ae26b
mt5225 <mt5225@gmail.com>
2017-09-25 17:18:08 -0700

    add log

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import numpy as np
 import glob
 from jinja2 import Environment, FileSystemLoader
 import paramiko
+import logging
+
+# create logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def cleanup():
     ''' Clean old file
@@ -79,7 +83,6 @@ def fetch_data(file_list):
                    username='ubuntu',
                    key_filename='/Users/mt5225/pem/website_en.pem')
     sftp = client.open_sftp()
-
     result = [sftp.get('/tmp/data/' + i, './data/' + i) for i in file_list]
     sftp.close()
 
@@ -97,6 +100,7 @@ def run_stat_on_server():
     client.connect('52.27.170.134',
                    username='ubuntu',
                    key_filename='/Users/mt5225/pem/website_en.pem')
+    client.exec_command('rm -rf /tmp/data/*.csv');
     client.exec_command('/bin/sh /home/ubuntu/scripts/weeklyreport2.sh');
     client.close();
 
@@ -126,23 +130,37 @@ def main():
     ''' Main entry
     '''
     # init
+    logging.info("cleanup old files")
     cleanup()
     file_list = ['summary.csv', 'user_weekcount.csv',
                  'scene_weekcount.csv', 'top10_user.csv']
     pdf_filename = 'uDCB_Weekly_Report_' + \
         strftime("%Y_%m_%d", gmtime()) + '.pdf'
     weekday = strftime("%Y_%m_%d", gmtime())
-
+    logging.info("generate data file")
     run_stat_on_server()
+    logging.info("sleep for 3 secs")
+    sleep(3)
+
+    logging.info("fetch data file")
     fetch_data(file_list)
 
     # generate chart file from data
+    logging.info("generate stat chart")
     map(generate_image, [item[:-4] for item in file_list[1:]])
+
+    logging.info("sleep for 3 secs")
+    sleep(3)
+
+    logging.info("generate download chart")
     generate_download_chart(get_downloads())
     
     # generate pdf
+    logging.info("generate pdf")
     generate_pdf(pdf_filename, weekday)
     move_to_desktop(pdf_filename)
+
+    logging.info("== DONE ==")
 
 if __name__ == "__main__":
     main()
